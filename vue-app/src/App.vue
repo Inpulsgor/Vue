3dddea1b0f07979c815234defa4ef6c6762a178a
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-02-04 00:46:06 +0200

    vue-app

---
+++ vue-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    {{ title }}
+    <Button type="button">Click me</Button>
+  </div>
+</template>
+
+<script>
+import Button from './components/Button.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      click: 0
+    }
+  },
+  computed: {
+    title() {
+      return `amount of clicks ${this.click}`
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
